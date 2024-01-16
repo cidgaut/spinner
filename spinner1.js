6c5df79d741d1c3a6332b7859119d2bb07283e7e
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 14:54:42 -0500

    animation added

---
+++ spinner1.js
@@ -0,0 +1,41 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+//200 incriments 
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+//last character is the same as starting for full circle effect
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
